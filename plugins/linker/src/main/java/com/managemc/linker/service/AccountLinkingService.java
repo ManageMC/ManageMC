a8af8489774eb2877d0a515206330619b129516f
Jacob Crofts <rmrfinbox@gmail.com>
2022-08-28 15:38:25 -0700

    Consume simplified API

--- plugins/linker/src/main/java/com/managemc/linker/service/AccountLinkingService.java
+++ plugins/linker/src/main/java/com/managemc/linker/service/AccountLinkingService.java
@@ -13,7 +13,6 @@ public class AccountLinkingService {
   public static final String SUCCESS_MSG = ChatColor.GREEN + "Account linked successfully. If you need to link another Minecraft account, you will need to use a new token.";
   public static final String WRONG_TOKEN_MSG = ChatColor.RED + "Wrong account linking token";
   public static final String ALREADY_LINKED_MSG = ChatColor.RED + "A different user has already linked this account";
-  public static final String EMAIL_NOT_FOUND_MSG = ChatColor.RED + "Email address not found";
   public static final String UNEXPECTED_EXCEPTION = ChatColor.DARK_RED + "Unexpected plugin exception";
 
   private final BukkitLogging logger;
@@ -24,9 +23,8 @@ public class AccountLinkingService {
     this.clientProvider = clientProvider;
   }
 
-  public void linkPlayer(Player sender, String email, String token) {
+  public void linkPlayer(Player sender, String token) {
     LinkPlayerInput input = new LinkPlayerInput()
-        .email(email)
         .accountLinkingToken(token)
         .uuid(sender.getUniqueId());
 
@@ -49,8 +47,6 @@ public class AccountLinkingService {
         return WRONG_TOKEN_MSG;
       case ALREADY_LINKED:
         return ALREADY_LINKED_MSG;
-      case USER_NOT_FOUND:
-        return EMAIL_NOT_FOUND_MSG;
       default:
         throw new RuntimeException("Unexpected LinkPlayerResponse status " + response.getStatus());
     }
