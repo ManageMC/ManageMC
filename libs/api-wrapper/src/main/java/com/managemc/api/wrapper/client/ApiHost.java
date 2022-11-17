67573b4955ad817a02d62f8cb69792e48a588701
Jacob Crofts <rmrfinbox@gmail.com>
2022-11-17 14:58:29 -0600

    Remove bundled certificate

--- libs/api-wrapper/src/main/java/com/managemc/api/wrapper/client/ApiHost.java
+++ libs/api-wrapper/src/main/java/com/managemc/api/wrapper/client/ApiHost.java
@@ -4,20 +4,17 @@ import lombok.Getter;
 
 public enum ApiHost {
 
-  DEVELOPMENT(0, "http://localhost:9070/api/v1", "demo-api.crt"),
-  DEMO(1, "https://api-demo.managemc.com/api/v1", "demo-api.crt"),
-  PRODUCTION(2, "https://api.managemc.com/api/v1", "api.crt");
+  DEVELOPMENT(0, "http://localhost:9070/api/v1"),
+  DEMO(1, "https://api-demo.managemc.com/api/v1"),
+  PRODUCTION(2, "https://api.managemc.com/api/v1");
 
   @Getter
   private final int serverIndex;
   @Getter
   private final String baseUrl;
-  @Getter
-  private final String bundledCert;
 
-  ApiHost(int serverIndex, String baseUrl, String bundledCert) {
+  ApiHost(int serverIndex, String baseUrl) {
     this.serverIndex = serverIndex;
     this.baseUrl = baseUrl;
-    this.bundledCert = bundledCert;
   }
 }
